c17be1c6d1c61cd756d6a72ca2d3e535fc93027f
matthew2856 <g1166258@study.ouhk.edu.hk>
2019-01-25 15:33:07 +0800

    one doc

--- server.js
+++ server.js
@@ -2,16 +2,18 @@
 var express = require('express');
 var app = express();
 var assert = require('assert');
+var Schema = mongoose.Schema;
 
 var MongoClient = require('mongodb').MongoClient;
 var mongourl = 'mongodb://testuser:testing123@ds151814.mlab.com:51814/fyp_data';
 
 app.use('/', express.static('public/public'));
 
+
 app.get('/getgps', function (req, res) {
 	MongoClient.connect(mongourl, function (err, db) {
         if (err) throw err;
-		db.collection("newgpsdata").find().toArray(function (err, docs) {
+		db.collection("newjson").find().toArray(function (err, docs) {
 			assert.equal(err, null);
 			db.close()
 			if (docs != null) {
@@ -28,9 +30,9 @@ app.get('/getgps', function (req, res) {
 	})
 })
 
-
 app.get('/', function (req, res) {
 		res.redirect('/getgps');
 })
+
 
 app.listen(process.env.PORT || 8099);
